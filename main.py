9c3faec77e5403614b1d7e6990aa947ae1da5515
zderidder <zach.deridder@gmail.com>
2021-04-27 19:05:38 -0500

    Ok actually fixed it now

--- main.py
+++ main.py
@@ -14,13 +14,9 @@ def aboutus():
 def contactus():
     return render_template("contact_us.html")
 
-@app.route("/restaurants")
-def restaurants():
-    return render_template("restaurants.html")
-
-@app.route("/recipes")
-def recipes():
-    return render_template("homemade.html")
+@app.route("/fried")
+def fried():
+    return render_template("fried.html")
 
 @app.route("/fun-facts")
 def funfacts():
@@ -29,6 +25,22 @@ def funfacts():
 @app.route("/history")
 def history():
     return render_template("history.html")
+
+@app.route("/homemade")
+def homemade():
+    return render_template("homemade.html")
+
+@app.route("/pasta")
+def pasta():
+    return render_template("pasta.html")
+
+@app.route("/restaurants")
+def restaurants():
+    return render_template("restaurants.html")
+
+@app.route("/soup")
+def soup():
+    return render_template("soup.html")
 
 if __name__=='__main__':
     app.run(debug=True)
